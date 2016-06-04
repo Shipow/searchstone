f07cfd1731c35f0664d7d93891644bccac3de65f
Shipow <shipowlata@gmail.com>
2016-06-04 16:52:15 +0200

    sunwell light

--- src/js/sunwell.js
+++ src/js/sunwell.js
@@ -109,27 +109,6 @@
         sunwell.init();
     }
 
-    sunwell.races = {
-        'enUS': {
-            'MURLOC': 'Murloc',
-            'MECHANICAL': 'Mech',
-            'BEAST': 'Beast',
-            'DEMON': 'Demon',
-            'PIRATE': 'Pirate',
-            'DRAGON': 'Dragon',
-            'TOTEM': 'Totem'
-        },
-        "deDE": {
-            'MURLOC': 'Murloc',
-            'MECHANICAL': 'Mech',
-            'BEAST': 'Wildtier',
-            'DEMON': 'Dämon',
-            'PIRATE': 'Pirat',
-            'DRAGON': 'Drache',
-            'TOTEM': 'Totem'
-        }
-    };
-
     /**
      * Calculate the checksum for an object.
      * @param o
@@ -159,7 +138,7 @@
      * @param h
      */
     function drawEllipse(ctx, x, y, w, h) {
-        var kappa = .5522848,
+        var kappa = 0.5522848,
             ox = (w / 2) * kappa, // control point offset horizontal
             oy = (h / 2) * kappa, // control point offset vertical
             xe = x + w,           // x-end
@@ -258,7 +237,6 @@
                             }
                         } else {
                             srcURL = sunwell.settings.assetFolder + key + '.png';
-                            ;
                         }
                         log('Requesting ' + srcURL);
                         assets[key].src = srcURL;
@@ -350,64 +328,6 @@
             }
 
         return {x: minX, y: minY, maxX: maxX, maxY: maxY, w: maxX - minX, h: maxY - minY};
-    }
-
-    function renderRaceText(targetCtx, s, card) {
-        var text, x;
-
-        if (sunwell.races[card.language]) {
-            if (sunwell.races[card.language][card.race]) {
-                text = sunwell.races[card.language][card.race];
-            } else {
-                text = card.race;
-            }
-        } else {
-            if (sunwell.races['enUS'][card.race]) {
-                text = sunwell.races['enUS'][card.race];
-            } else {
-                text = card.race;
-            }
-        }
-
-        var buffer = getBuffer();
-        var bufferCtx = buffer.getContext('2d');
-
-        buffer.width = 300;
-        buffer.height = 60;
-
-        bufferCtx.font = '45px Belwe';
-
-        bufferCtx.lineCap = 'round';
-        bufferCtx.lineJoin = 'round';
-        bufferCtx.textBaseline = 'hanging';
-
-        bufferCtx.textAlign = 'left';
-
-        text = text.split('');
-
-        x = 10;
-
-        for (var i = 0; i < text.length; i++) {
-            bufferCtx.lineWidth = 8;
-            bufferCtx.strokeStyle = 'black';
-            bufferCtx.fillStyle = 'black';
-            bufferCtx.fillText(text[i], x, 10);
-            bufferCtx.strokeText(text[i], x, 10);
-
-            bufferCtx.fillStyle = 'white';
-            bufferCtx.strokeStyle = 'white';
-            bufferCtx.lineWidth = 1;
-            bufferCtx.fillText(text[i], x, 10);
-            //ctx.strokeText(text[i], x, y);
-
-            x += bufferCtx.measureText(text[i]).width;
-        }
-
-        var b = contextBoundingBox(bufferCtx);
-
-        targetCtx.drawImage(buffer, b.x, b.y, b.w, b.h, (394 - (b.w / 2)) * s, (1001 - (b.h / 2)) * s, b.w * s, b.h * s);
-
-        freeBuffer(buffer);
     }
 
     /**
@@ -528,384 +448,6 @@
         return [xPos, yPos];
     }
 
-    /**
-     * Renders the HTML body text of a card.
-     * @param targetCtx
-     * @param s
-     * @param card
-     */
-    function drawBodyText(targetCtx, s, card, forceSmallerFirstLine) {
-        var manualBreak = card.text.substr(0, 3) === '[x]',
-            bufferText = getBuffer(),
-            bufferTextCtx = bufferText.getContext('2d'),
-            bufferRow = getBuffer(),
-            bufferRowCtx = bufferRow.getContext('2d'),
-            bodyText = manualBreak ? card.text.substr(3) : card.text,
-            words,
-            word,
-            chars,
-            char,
-            width,
-            spaceWidth,
-            xPos = 0,
-            yPos = 0,
-            isBold = 0,
-            isItalic = 0,
-            i,
-            j,
-            r,
-            centerLeft,
-            centerTop,
-            justLineBreak,
-            lineCount = 0,
-            plurals,
-            pBodyText;
-
-
-        pBodyText = bodyText;
-        while((plurals = pluralRegex.exec(bodyText)) !== null){
-            pBodyText = pBodyText.replace(plurals[0], plurals[1] + plurals[2] + (parseInt(plurals[1], 10) === 1 ? plurals[3] : plurals[4]));
-        }
-        bodyText = pBodyText;
-
-        words = bodyText.replace(/[\$#_]/g, '').split(/( |\n)/g);
-
-        log('Rendering body: ' + bodyText);
-
-        centerLeft = 390;
-        centerTop = 860;
-        bufferText.width = 520;
-        bufferText.height = 290;
-
-        bufferRow.width = 520;
-
-        if (card.type === 'SPELL') {
-            bufferText.width = 460;
-            bufferText.height = 290;
-
-            bufferRow.width = 460;
-        }
-
-        if (card.type === 'WEAPON') {
-            bufferText.width = 470;
-            bufferText.height = 250;
-
-            bufferRow.width = 470;
-        }
-
-        var fontSize = sunwell.settings.bodyFontSize;
-        var lineHeight = sunwell.settings.bodyLineHeight;
-        var totalLength = card.text.replace(/<\/*.>/g, '').length;
-        var smallerFirstLine = false;
-
-        if (totalLength >= 80) {
-            fontSize = sunwell.settings.bodyFontSize * 0.9;
-            lineHeight = sunwell.settings.bodyLineHeight * 0.9;
-        }
-
-        if (totalLength >= 100) {
-            fontSize = sunwell.settings.bodyFontSize * 0.8;
-            lineHeight = sunwell.settings.bodyLineHeight * 0.8;
-        }
-
-        bufferRow.height = lineHeight;
-
-
-        if (totalLength >= 75 && card.type === 'SPELL') {
-            smallerFirstLine = true;
-        }
-
-        if(forceSmallerFirstLine){
-            smallerFirstLine = true;
-        }
-
-        if (card.type === 'WEAPON') {
-            bufferRowCtx.fillStyle = '#fff';
-        } else {
-            bufferRowCtx.fillStyle = '#000';
-        }
-        bufferRowCtx.textBaseline = 'hanging';
-
-        bufferRowCtx.font = fontSize + 'px/1em "' + sunwell.settings.bodyFont + '", sans-serif';
-
-        spaceWidth = 3;
-
-        for (i = 0; i < words.length; i++) {
-            word = words[i];
-
-            chars = word.split('');
-
-            width = bufferRowCtx.measureText(word).width;
-            log('Next word: ' + word);
-
-            if (!manualBreak && (xPos + width > bufferRow.width || (smallerFirstLine && xPos + width > bufferRow.width * 0.8))) {
-                log((xPos + width) + ' > ' + bufferRow.width);
-                log('Calculated line break');
-                smallerFirstLine = false;
-                lineCount++;
-                r = finishLine(bufferTextCtx, bufferRow, bufferRowCtx, xPos, yPos, bufferText.width);
-                xPos = r[0];
-                yPos = r[1];
-                justLineBreak = true;
-            }
-
-            for (j = 0; j < chars.length; j++) {
-                char = chars[j];
-
-                log(char + ' ' + char.charCodeAt(0));
-
-                if (char.charCodeAt(0) === 10) {
-                    if (justLineBreak) {
-                        justLineBreak = false;
-                        continue;
-                    }
-                    lineCount++;
-                    r = finishLine(bufferTextCtx, bufferRow, bufferRowCtx, xPos, yPos, bufferText.width);
-                    xPos = r[0];
-                    yPos = r[1];
-                    log('Manual line break');
-                    continue;
-                }
-
-                justLineBreak = false;
-
-                if (char === '<') {
-                    if (chars[j + 1] === '/') {
-                        if (chars[j + 2] === 'b') {
-                            isBold--;
-                            j += 3;
-                        }
-
-                        if (chars[j + 2] === 'i') {
-                            isItalic--;
-                            j += 3;
-                        }
-                    } else {
-                        if (chars[j + 1] === 'b') {
-                            isBold++;
-                            j += 2;
-                        }
-
-                        if (chars[j + 1] === 'i') {
-                            isItalic++;
-                            j += 2;
-                        }
-                    }
-
-                    bufferRowCtx.font = (isBold > 0 ? 'bold ' : '') + (isItalic > 0 ? 'italic' : '') + ' ' + fontSize + 'px/1em "' + sunwell.settings.bodyFont + '", sans-serif';
-                    continue;
-                }
-
-                bufferRowCtx.fillText(char, xPos + sunwell.settings.bodyFontOffset.x, sunwell.settings.bodyFontOffset.y);
-
-                xPos += bufferRowCtx.measureText(char).width + (spaceWidth / 8);
-            }
-
-            xPos += spaceWidth;
-        }
-
-        lineCount++;
-        finishLine(bufferTextCtx, bufferRow, bufferRowCtx, xPos, yPos, bufferText.width);
-
-        freeBuffer(bufferRow);
-
-        if(card.type === 'SPELL' && lineCount === 4){
-            if(!smallerFirstLine && !forceSmallerFirstLine){
-                drawBodyText(targetCtx, s, card, true);
-                return;
-            }
-        }
-
-        var b = contextBoundingBox(bufferTextCtx);
-
-        b.h = Math.ceil(b.h / bufferRow.height) * bufferRow.height;
-
-        targetCtx.drawImage(bufferText, b.x, b.y - 2, b.w, b.h, (centerLeft - (b.w / 2)) * s, (centerTop - (b.h / 2)) * s, b.w * s, (b.h + 2) * s);
-
-        freeBuffer(bufferText);
-
-        if (sunwell.settings.debug) {
-            targetCtx.save();
-            targetCtx.strokeStyle = 'green';
-            targetCtx.beginPath();
-            targetCtx.rect((centerLeft - (b.w / 2)) * s, (centerTop - (b.h / 2)) * s, b.w * s, (b.h + 2) * s);
-            targetCtx.stroke();
-            targetCtx.strokeStyle = 'red';
-            targetCtx.beginPath();
-            targetCtx.rect((centerLeft - (bufferText.width / 2)) * s, (centerTop - (bufferText.height / 2)) * s, bufferText.width * s, (bufferText.height + 2) * s);
-            targetCtx.stroke();
-            targetCtx.restore();
-        }
-    }
-
-    /**
-     * Given a curve and t, the function returns the point on the curve.
-     * r is the rotation of the point in radians.
-     * @param curve
-     * @param t
-     * @returns {{x: (number|*), y: (number|*), r: number}}
-     */
-    function getPointOnCurve(curve, t) {
-
-        var rX, rY, x, y;
-
-        rX = 3 * Math.pow(1 - t, 2) * (curve[1].x - curve[0].x) + 6 * (1 - t) * t * (curve[2].x - curve[1].x) + 3 * Math.pow(t, 2) * (curve[3].x - curve[2].x);
-        rY = 3 * Math.pow(1 - t, 2) * (curve[1].y - curve[0].y) + 6 * (1 - t) * t * (curve[2].y - curve[1].y) + 3 * Math.pow(t, 2) * (curve[3].y - curve[2].y);
-
-        x = Math.pow((1 - t), 3) * curve[0].x + 3 * Math.pow((1 - t), 2) * t * curve[1].x + 3 * (1 - t) * Math.pow(t, 2) * curve[2].x + Math.pow(t, 3) * curve[3].x;
-        y = Math.pow((1 - t), 3) * curve[0].y + 3 * Math.pow((1 - t), 2) * t * curve[1].y + 3 * (1 - t) * Math.pow(t, 2) * curve[2].y + Math.pow(t, 3) * curve[3].y;
-
-        return {
-            x: x,
-            y: y,
-            r: Math.atan2(rY, rX)
-        }
-    }
-
-    /**
-     * Prints the title of a card.
-     * @param title
-     */
-    function drawCardTitle(targetCtx, s, card) {
-        var buffer = getBuffer();
-        var title = card.title;
-        buffer.width = 1024;
-        buffer.height = 200;
-        var ctx = buffer.getContext('2d');
-        var boundaries;
-        ctx.save();
-
-        var pathMiddle = .58;
-        var maxWidth = 580;
-
-        //Path midpoint at t = 0.56
-        var c = [
-            {x: 0, y: 110},
-            {x: 102, y: 137},
-            {x: 368, y: 16},
-            {x: 580, y: 100}
-        ];
-
-        if (card.type === 'SPELL') {
-            pathMiddle = .52;
-            maxWidth = 580;
-            c = [
-                {x: 10, y: 100},
-                {x: 212, y: 35},
-                {x: 368, y: 35},
-                {x: 570, y: 105}
-            ]
-        }
-
-        if (card.type === 'WEAPON') {
-            pathMiddle = .58;
-            maxWidth = 580;
-            c = [
-                {x: 10, y: 75},
-                {x: 50, y: 75},
-                {x: 500, y: 75},
-                {x: 570, y: 75}
-            ]
-        }
-
-        var fontSize = 51;
-
-        ctx.lineWidth = 13;
-        ctx.strokeStyle = 'black';
-
-        ctx.lineCap = 'round';
-        ctx.lineJoin = 'round';
-
-        ctx.textAlign = 'left';
-        ctx.textBaseline = 'middle';
-
-        var textWidth = maxWidth + 1;
-        var steps,
-            begin;
-
-        while (textWidth > maxWidth && fontSize > 10) {
-            fontSize -= 1;
-            ctx.font = fontSize + 'px Belwe';
-            textWidth = 0;
-            for (var i = 0; i < title.length; i++) {
-                textWidth += ctx.measureText(title[i]).width + 2;
-            }
-
-            textWidth *= 1.25;
-        }
-
-        textWidth = textWidth / maxWidth;
-        begin = pathMiddle - (textWidth / 2);
-        steps = textWidth / title.length;
-
-        if (sunwell.settings.debug) {
-            ctx.save();
-            ctx.strokeStyle = 'red';
-            ctx.lineWidth = 5;
-            ctx.beginPath();
-            ctx.moveTo(c[0].x, c[0].y);
-            ctx.bezierCurveTo(c[1].x, c[1].y, c[2].x, c[2].y, c[3].x, c[3].y);
-            ctx.stroke();
-            ctx.restore();
-        }
-
-        var p, t, leftPos = 0, m;
-        for (i = 0; i < title.length; i++) {
-            if (leftPos === 0) {
-                t = begin + (steps * i);
-                p = getPointOnCurve(c, t);
-                leftPos = p.x;
-            } else {
-                t += 0.01;
-                p = getPointOnCurve(c, t);
-                while (p.x < leftPos) {
-                    t += 0.001;
-                    p = getPointOnCurve(c, t);
-                }
-            }
-
-            ctx.save();
-            ctx.translate(p.x, p.y);
-
-            ctx.scale(1.2, 1);
-            //ctx.setTransform(1.2, p.r, 0, 1, p.x, p.y);
-            ctx.rotate(p.r);
-
-            ctx.lineWidth = 10 * (fontSize / 50);
-            ctx.strokeStyle = 'black';
-            ctx.fillStyle = 'black';
-            ctx.fillText(title[i], 0, 0);
-            ctx.strokeText(title[i], 0, 0);
-            m = ctx.measureText(title[i]).width * 1.25;
-            leftPos += m;
-
-            if (['i', 'f'].indexOf(title[i]) !== -1) {
-                leftPos += m * 0.1;
-            }
-
-            ctx.fillStyle = 'white';
-            ctx.strokeStyle = 'white';
-            ctx.lineWidth = 2.5 * (fontSize / 50);
-            ctx.fillText(title[i], 0, 0);
-
-            ctx.restore();
-        }
-
-        targetCtx.drawImage(
-            buffer,
-            0,
-            0,
-            580,
-            200,
-
-            (395 - (580 / 2)) * s,
-            (725 - 200) * s,
-            580 * s,
-            200 * s
-        );
-
-        freeBuffer(buffer);
-    }
 
     function draw(cvs, ctx, card, s, callback, internalCB) {
 
@@ -989,7 +531,7 @@
             ctx.shadowBlur=50*s;
             ctx.shadowColor='#FF7275';
             ctx.shadowOffsetX = 1000;
-            ctx.globalAlpha = .5;
+            ctx.globalAlpha = 0.5;
             ctx.drawImage(getAsset('health'), 0, 0, 167, 218, (24 * s)-1000, 62 * s, 167 * s, 218 * s);
             ctx.restore();
         } else {
@@ -1067,41 +609,9 @@
         }
 
         drawProgress = 9;
-
-
         drawProgress = 10;
-
-        //drawNumber(ctx, 116, 170, s, card.cost || 0, 170, card.costStyle);
-
         drawProgress = 11;
-
-        //drawCardTitle(ctx, s, card);
-
         drawProgress = 12;
-
-        // if (card.type === 'MINION') {
-        //     if (card.race) {
-        //         renderRaceText(ctx, s, card);
-        //     }
-        //
-        //     drawNumber(ctx, 128, 994, s, card.attack || 0, 150, card.attackStyle);
-        //     drawNumber(ctx, 668, 994, s, card.health || 0, 150, card.healthStyle);
-        // }
-        //
-        // drawProgress = 13;
-        //
-        // if (card.type === 'WEAPON') {
-        //     drawNumber(ctx, 128, 994, s, card.attack || 0, 150, card.attackStyle);
-        //     drawNumber(ctx, 668, 994, s, card.durability || 0, 150, card.durabilityStyle);
-        // }
-        //
-        // drawProgress = 14;
-        //
-        // if (!card.silenced || card.type !== 'MINION') {
-        //     drawBodyText(ctx, s, card);
-        // } else {
-        //     ctx.drawImage(getAsset('silence-x'), 0, 0, 410, 397, 200 * s, 660 * s, 410 * s, 397 * s);
-        // }
 
         ctx.restore();
 
@@ -1224,7 +734,7 @@
 
 
         if (typeof card.texture === 'string' && card.set !== 'CHEAT') {
-            if (s <= .5) {
+            if (s <= 0.5) {
                 loadList.push('h:' + card.texture);
             } else {
                 loadList.push('t:' + card.texture);
@@ -1256,7 +766,7 @@
                     freeBuffer(cvs);
                 });
             });
-    };
+    }
 
     /**
      * This will flush sunwell's render caches.
@@ -1321,7 +831,6 @@
             renderTarget.src = renderCache[cacheKey] = result.toDataURL();
         });
 
-
         return {
             target: renderTarget,
             redraw: function () {
@@ -1350,5 +859,5 @@
                 });
             }
         };
-    }
+    };
 })();
