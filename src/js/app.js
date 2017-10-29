6ee0aedee38ada32fd90798448cc5fdf0d4d14b5
bobylito <bobylito@gmail.com>
2017-10-29 16:20:38 +0100

    feat(app): add service worker

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,4 @@ require('./sunwell.js');
 require('./analytics.js');
 require('./ui.js');
 require('./algolia-instantsearch-conf.js');
+require('./installSW.js');
---
+++ src/js/installSW.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator && window.location.protocol === 'https:') {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/js/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
