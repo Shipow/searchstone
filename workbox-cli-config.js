815e1576941f70479b884284ce3a03a3ad1f371a
bobylito <bobylito@gmail.com>
2017-11-26 03:03:52 +0100

    fix: use workbox for service worker related work

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "build/",
+  "globPatterns": [
+    "**/*.{css,png,ico,ttf,woff,woff2,svg,html,js,map,json,xml}"
+  ],
+  "swDest": "build/js/sw.js",
+  "globIgnores": [
+    "../workbox-cli-config.js"
+  ]
+};
